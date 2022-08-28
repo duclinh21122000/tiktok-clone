5b1339f5c5817c1575a79017a82d4340ae1a46a0
duclinh21122000 <duongduclinh2207@gmail.com>
2022-08-28 23:59:16 +0700

    [tiktok-clone] istall and setting customize-cra & babel-plugin-module-resolver

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,10 @@
+import myAlert from '@/myAlert'
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button onClick={myAlert}> Click me! </button>
+  )
+}
+
+export default Button
